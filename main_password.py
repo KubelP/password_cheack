518b8d47b2c00e9db43ec09caca361b67d82008c
KubelP <101816112+KubelP@users.noreply.github.com>
2022-04-01 21:35:40 +0200

    Add files via upload

---
+++ main_password.py
@@ -0,0 +1,20 @@
+'''main conects clases PasworBuild with API'''
+
+from password_check import PasswordBuild
+from check_password_api import API
+
+
+def build_checking():
+    '''functions runs in loop class PasswordBuild and API. \
+    Takes password to check from file and returns verifide passwords in file password_git.txt'''
+    with open('passwords_to_cheack.txt', mode = 'r', encoding = 'utf-8') as file:
+        for line in file:
+            password_cheack_level_1 = PasswordBuild(line)
+            password_cheack_level_1.password_checking()
+
+    with open('password_checked.txt', mode = 'r', encoding = 'utf-8') as file2:
+        for line2 in file2:
+            password_cheack_level_2 = API(line2)
+            password_cheack_level_2.leacked_password_checking()
+
+build_checking()
